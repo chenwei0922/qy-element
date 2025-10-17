74209550fb6979d036b2384624a9b4519b523a7c
Eleven <chen158_7@126.com>
2025-10-17 15:08:41 +0800

    fix: tes t

--- packages/chenwei02/index.ts
+++ packages/chenwei02/index.ts
@@ -1,7 +1,7 @@
 import 'virtual:svg-icons-register'
 import installer from './default'
 
-export * from '@chenwei02/components'
+// export * from '@chenwei02/components'
 export * from './make-installer'
 
 export const install = installer.install
